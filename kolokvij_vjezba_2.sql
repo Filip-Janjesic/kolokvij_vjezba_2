5283766d0624c8b4a6075af97fadf3b344bba390
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2021-05-23 21:21:19 +0200

    napravljena tablica brat

--- kolokvij_vjezba_2.sql
+++ kolokvij_vjezba_2.sql
@@ -2,3 +2,15 @@ drop database if exists kolokvij_vjezba_2;
 create database kolokvij_vjezba_2;
 use kolokvij_vjezba_2;
 
+create table brat(
+	sifra int not null primary key auto_increment,
+	suknja varchar(47),
+	ogrlice int not null,
+	asocijalno bit not null,
+	neprijatelj int not null
+);
+
+create table neprijatelj(
+	sifra int not null primary key auto_increment,
+	
+);
