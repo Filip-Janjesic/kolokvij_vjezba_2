e4bfe29b40276e07564ef41361a68a745011cbfc
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2021-05-23 22:20:30 +0200

    tablice napravljene

--- kolokvij_vjezba_2.sql
+++ kolokvij_vjezba_2.sql
@@ -12,5 +12,49 @@ create table brat(
 
 create table neprijatelj(
 	sifra int not null primary key auto_increment,
-	
+	majca varchar(32),
+	haljina varchar(43) not null,
+	lipa decimal (16.8),
+	modelnaocala varchar(49) not null,
+	kuna decimal(12.6) not null,
+	jmbag char(11),
+	cura int
 );
+
+create table cura ( 
+	sifra int not null primary key auto_increment,
+	haljina varchar(33) not null,
+	drugiputa datetime not null,
+	suknja varchar (38),
+	narukvica int,
+	introvertno bit,
+	majica varchar(40) not null,
+	decko int
+);
+
+create table decko(
+	sifra int not null primary key auto_increment,
+	indiferentno bit,
+	vesta varchar(34) not null,
+	asocijalno bit not null
+);
+
+create table decko_zarucnica(
+	sifra int not null primary key auto_increment,
+	decko int not null,
+	zarucnica int not null
+);
+
+create table zarucnica(
+	sifra int not null primary key auto_increment,
+	narukvica int,
+	bojakose varchar(37) not null,
+	novcica decimal(15.9),
+	lipa decimal(15.98) not null,
+	indiferentno bit not null
+);
+
+
+
+
+
